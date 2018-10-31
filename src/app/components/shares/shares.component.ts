628a9d215d7275f4c951a94224c1a7b8a562e2db
xamfy <spratyaksh22@gmail.com>
2018-11-01 00:57:46 +0530

    Shares component complete!

--- src/app/components/shares/shares.component.ts
+++ src/app/components/shares/shares.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from 'src/app/services/data.service';
+import { formatDate } from '@angular/common';
 
 @Component({
   selector: 'app-shares',
@@ -10,19 +11,40 @@ export class SharesComponent implements OnInit {
 
   public data;
   search_term: string;
+  curDate: string = formatDate(new Date(), 'yyyy-MM-dd', 'en');
+  output: string;
+  index: string;
+  cDate: Date;
 
-  constructor(private dataService: DataService) {}
-  ngOnInit() {
-    // this.dataService.getShareData().subscribe((data) => {
-    //   this.data = data['Time Series (Daily)'];
-    //   console.log(data['Time Series (Daily)']);
-    // });
+  open: string;
+  high: string;
+  low: string;
+  close: string;
+  volume: string;
+
+  constructor(private dataService: DataService) {
+    // this.datePipe.transform(this.curDate, 'yyyy-MM-dd');
   }
+  ngOnInit() {}
 
   getPrice() {
     this.dataService.getShareData(this.search_term).subscribe((data) => {
-      console.log(data);
-      this.data = data;
+      if (data['Time Series (Daily)'][this.curDate] === undefined) {
+        this.cDate = new Date();
+        this.cDate.setDate(this.cDate.getDate() - 2);
+        this.curDate = formatDate(this.cDate, 'yyyy-MM-dd', 'en');
+        // console.log(this.curDate);
+        this.data = data['Time Series (Daily)'][this.curDate];
+        console.log(this.data);
+      } else {
+        this.data = data['Time Series (Daily)'][this.curDate];
+      }
+      // this.open = data[]
+      this.open = this.data['1. open'];
+      this.high = this.data['2. high'];
+      this.low = this.data['3. low'];
+      this.close = this.data['4. close'];
+      this.volume = this.data['5. volume'];
     });
   }
 }
